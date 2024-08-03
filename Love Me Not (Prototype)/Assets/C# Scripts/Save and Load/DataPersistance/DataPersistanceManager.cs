69aeb9110b4b401241dc96491c881bb84bc9991c
AlanMa21 <alan.ma233@gmail.com>
2024-08-03 23:16:17 +0800

    Save and Load
    Working on creating the save and load system, there's quite a bit of issues that needs to be fixed.

--- Love Me Not (Prototype)/Assets/C# Scripts/Save and Load/DataPersistance/DataPersistanceManager.cs
+++ Love Me Not (Prototype)/Assets/C# Scripts/Save and Load/DataPersistance/DataPersistanceManager.cs
@@ -6,14 +6,21 @@ using Unity.VisualScripting;
 
 public class DataPersistanceManager : MonoBehaviour
 {
+
+  [Header("File Storage Config")]
+  [SerializeField] private string fileName;
    public static DataPersistanceManager instance {get; private set;}
 
    private GameData gameData;
 
    private List<IDataPersistance> dataPersistancesObjects;
 
+   private FileDataHandler dataHandler;
+
    private void Start()
    { 
+
+       this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
        this.dataPersistancesObjects = FindAllDataPersistanceObjects();
        LoadGame();
    }
@@ -33,7 +40,9 @@ public class DataPersistanceManager : MonoBehaviour
    }
 
    public void LoadGame()
-   {
+   {    
+        this.gameData = dataHandler.Load();
+
         if (this.gameData == null)
         {
             Debug.Log("No data was found. Initialzing data to default.");
@@ -52,6 +61,10 @@ public class DataPersistanceManager : MonoBehaviour
       {
         dataPersistanceObj.SaveGame(ref gameData);
       }
+
+      dataHandler.Save(gameData);
+
+
    }
 
    private void OnApplicationQuit()
